93223533edc4184d7cfcdc3282f2638e5a37ce76
cmcgilve <cmcgilve@ualberta.ca>
2024-03-07 17:27:54 -0700

    Added hasCityTest()

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,7 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -28,5 +29,13 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
+    }
+
+    @Test
+    public void hasCityTest(){
+        list = MockCityList();
+        City testCity = new City("Estevan", "SK");
+        list.addCity(testCity);
+        assertTrue(list.hasCity(testCity));
     }
 }
